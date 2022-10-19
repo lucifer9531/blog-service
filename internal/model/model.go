22050039c61f327fbb37f12564598cba5a5544eb
Lucifer <baixp@dustess.com>
2022-10-19 11:31:21 +0800

    Initial commit

---
+++ internal/model/model.go
@@ -0,0 +1,41 @@
+package model
+
+import (
+	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/ruixijiejie/blog-service/global"
+	"github.com/ruixijiejie/blog-service/pkg/setting"
+)
+
+type Model struct {
+	ID         uint32 `gorm:"primary_key" json:"id"`
+	CreatedBy  string `json:"created_by"`
+	ModifiedBy string `json:"modified_by"`
+	CreatedOn  string `json:"created_on"`
+	ModifiedOn string `json:"modified_on"`
+	DeletedOn  string `json:"deleted_on"`
+	IsDel      uint8  `json:"is_del"`
+}
+
+func NewDBEngine(databaseSetting *setting.DatabaseSettingS) (*gorm.DB, error) {
+	s := "%s:%s@tcp(%s)/%s?charset=%s&parseTime=%t&loc=Local"
+	db, err := gorm.Open(databaseSetting.DBType, fmt.Sprintf(s,
+		databaseSetting.UserName,
+		databaseSetting.Password,
+		databaseSetting.Host,
+		databaseSetting.DBName,
+		databaseSetting.Charset,
+		databaseSetting.ParseTime,
+	))
+	if err != nil {
+		return nil, err
+	}
+	if global.ServerSetting.RunMode == "debug" {
+		db.LogMode(true)
+	}
+	db.SingularTable(true)
+	db.DB().SetMaxIdleConns(databaseSetting.MaxIdleConns)
+	db.DB().SetMaxOpenConns(databaseSetting.MaxOpenConns)
+	return db, nil
+}
---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/ruixijiejie/blog-service/global"
+	"github.com/ruixijiejie/blog-service/internal/model"
+	"github.com/ruixijiejie/blog-service/internal/routers"
+	"github.com/ruixijiejie/blog-service/pkg/setting"
+	"log"
+	"net/http"
+	"time"
+)
+
+func init() {
+	err := setupSetting()
+	if err != nil {
+		log.Fatalf("init.setupSetting err: %v", err)
+	}
+	err = setupDBEngine()
+	if err != nil {
+		log.Fatalf("init.setupDBEngine err: %v", err)
+	}
+}
+
+func main() {
+	gin.SetMode(global.ServerSetting.RunMode)
+	router := routers.NewRouter()
+	s := &http.Server{
+		Addr:           ":" + global.ServerSetting.HttpPort,
+		Handler:        router,
+		ReadTimeout:    global.ServerSetting.ReadTimeout,
+		WriteTimeout:   global.ServerSetting.WriteTimeout,
+		MaxHeaderBytes: 1 << 20,
+	}
+	err := s.ListenAndServe()
+	if err != nil {
+		log.Fatalf("background service start failed err: %v", err)
+	}
+}
+
+func setupSetting() error {
+	newSetting, err := setting.NewSetting()
+	if err != nil {
+		return err
+	}
+	err = newSetting.ReadSection("Server", &global.ServerSetting)
+	if err != nil {
+		return err
+	}
+	err = newSetting.ReadSection("App", &global.AppSetting)
+	if err != nil {
+		return err
+	}
+	err = newSetting.ReadSection("Database", &global.DatabaseSetting)
+	if err != nil {
+		return err
+	}
+	global.ServerSetting.ReadTimeout *= time.Second
+	global.ServerSetting.WriteTimeout *= time.Second
+	return nil
+}
+
+func setupDBEngine() error {
+	var err error
+	global.DBEngine, err = model.NewDBEngine(global.DatabaseSetting)
+	if err != nil {
+		return err
+	}
+	return nil
+}
